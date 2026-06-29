e8139fe2688d4f4733cca521cfd7b7c1f7b27d84
Andrew Carver <Andrew.Carver.dev@gmail.com>
2026-06-29 06:10:25 -0400

    fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,18 @@ return {
     local treesitterConfig = require("nvim-treesitter.configs")
     treesitterConfig.setup({
       auto_install = true,
-      ensure_installed = { "lua", "javascript", "markdown" },
+      ensure_installed = { 
+    "lua", 
+    "vim", 
+    "vimdoc", 
+    "query", 
+    "markdown", 
+    "markdown_inline",
+    "xml",     
+    "json", 
+    "yaml", 
+    "javascript", 
+    "markdown" },
       highlight = { enable = true },
       indent = { enable = true },
     })
