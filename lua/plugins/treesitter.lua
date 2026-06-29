4039d464f328733d9f3d15dff2831862889c389c
Andrew Carver <Andrew.Carver.dev@gmail.com>
2026-06-29 06:07:29 -0400

    fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  branch = "main",
   config = function()
     local treesitterConfig = require("nvim-treesitter.configs")
     treesitterConfig.setup({
